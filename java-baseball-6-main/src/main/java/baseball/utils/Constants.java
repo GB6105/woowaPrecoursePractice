3a9e63254ce7b0829ce8d47430b7315fdcba3662
Tae Kyoun Kim <xorbskim6105@gmail.com>
2025-09-15 15:06:37 +0900

    feat/ add validator

---
+++ java-baseball-6-main/src/main/java/baseball/utils/Constants.java
@@ -0,0 +1,8 @@
+package baseball.utils;
+
+import java.util.regex.Pattern;
+
+public class Constants {
+
+  public static final Pattern INPUT_NUMBER = Pattern.compile("[0-9]{3}$");
+}
---
+++ java-baseball-6-main/src/main/java/baseball/validator/InputValidator.java
@@ -0,0 +1,17 @@
+package baseball.validator;
+
+import baseball.utils.Constants;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+
+  private InputValidator() {
+  }
+
+  public boolean inputValidate(String input) {
+    if (!Constants.INPUT_NUMBER.matcher(input).matches()) {
+      return false;
+    }
+    return true;
+  }
+}
